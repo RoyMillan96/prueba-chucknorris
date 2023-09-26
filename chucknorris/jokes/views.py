83e2b84768f2f8308d7a7a89e7876d7714aa2d33
Roy Millan <millan.rodrigo201@gmail.com>
2023-09-26 11:31:56 -0600

    Custom time response

--- chucknorris/jokes/views.py
+++ chucknorris/jokes/views.py
@@ -1,6 +1,8 @@
+import time
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from concurrent.futures import ThreadPoolExecutor
 from .integrations import get_fetch_jokes
 from .serializers import JokeSerializer
 
@@ -10,7 +12,7 @@ class JokeList(APIView):
         joke_ids = set()
         jokes = []
 
-        for _ in range(25):
+        def fetch_and_append(_):
             joke_id, joke_value = get_fetch_jokes()
             if joke_id not in joke_ids:
                 joke_ids.add(joke_id)
@@ -21,6 +23,8 @@ class JokeList(APIView):
                         'total': len(joke_ids)
                     }
                 )
+        with ThreadPoolExecutor(max_workers=25) as executor:
+            executor.map(fetch_and_append, range(25))
 
         serializer = JokeSerializer(jokes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
